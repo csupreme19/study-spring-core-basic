28b106fac04d073eb392ad6703230406d52e2cc9
csupreme19 <csupreme19@gmail.com>
2022-10-20 00:21:46 +0900

    회원 도메인 설계 🫠

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static ThreadLocal<Map<Long, Member>> threadLocalStore = new ThreadLocal<>();
+
+    @Override
+    public void save(Member member) {
+        threadLocalStore.get().put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return threadLocalStore.get().get(memberId);
+    }
+}
