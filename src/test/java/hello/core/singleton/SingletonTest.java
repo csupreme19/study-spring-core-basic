27f63539a08b1b039734cb26ab967a102090631c
csupreme19 <csupreme19@gmail.com>
2022-10-23 00:21:43 +0900

    싱글톤 미적용 DI 컨테이너 테스트

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,25 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SingletonTest {
+
+    @Test
+    public void pureContainer() throws Exception {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+}
