4eb80530c5ff473d6e07e7af20a563f50905a265
csupreme19 <csupreme19@gmail.com>
2022-10-25 01:01:59 +0900

    컴포넌트 스캔 필터

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
 public class AutoAppConfig {
 }
---
+++ src/test/java/hello/core/scan/ComponentFilterAppConfigTest.java
@@ -0,0 +1,41 @@
+package hello.core.scan;
+
+import hello.core.scan.filter.BeanA;
+import hello.core.scan.filter.BeanB;
+import hello.core.scan.filter.MyExcludeComponent;
+import hello.core.scan.filter.MyIncludeComponent;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertThat(beanA).isNotNull();
+
+        Executable runnable = () -> ac.getBean("beanB", BeanB.class);
+        assertThrows(NoSuchBeanDefinitionException.class, runnable);
+    }
+
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type= FilterType.ANNOTATION, classes = MyIncludeComponent.class)
+            , excludeFilters = @Filter(type= FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {
+
+    }
+}
