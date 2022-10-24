507a851b9605c0291ea6c9639e5b5339920672b5
csupreme19 <csupreme19@gmail.com>
2022-10-25 00:35:14 +0900

    컴포넌트 스캔과 컴포넌트

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     private static ThreadLocal<Map<Long, Member>> threadLocalStore = new ThreadLocal<>();
